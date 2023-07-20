4abd9ba29fa4206a1dfc9b738427f892ff174d4d
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-07-20 14:46:45 -0400

    Testing with local images

--- js/imgur-gallery.js
+++ js/imgur-gallery.js
@@ -1,20 +1,47 @@
 (function() {
    angular.module('imgurGalleryApp', [])
-      .controller('ImgurGalleryController', ['$http', function($http) {
+      .controller('ImgurGalleryController', function() {
          var vm = this;
-         vm.images = [];
-
-         // GET request to the Imgur API
-         $http.get('https://api.imgur.com/3/gallery/hot/', {
-            headers: {
-               'Authorization': 'Client-ID 81c3d04c80f0c3b'
+         vm.images = [
+            {
+               link: 'https://images.pexels.com/photos/1292843/pexels-photo-1292843.jpeg?auto=compress&cs=tinysrgb&w=600',
+               title: 'Image 1'
+            },
+            {
+               link: 'https://images.pexels.com/photos/1750754/pexels-photo-1750754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+               title: 'Image 2'
+            },
+            {
+               link: 'https://images.pexels.com/photos/1856488/pexels-photo-1856488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+               title: 'Image 3'
+            },
+            {
+               link: 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+               title: 'Image 4'
+            },
+            {
+               link: 'https://images.pexels.com/photos/1598072/pexels-photo-1598072.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+               title: 'Image 5'
+            },
+            {
+               link: 'https://images.pexels.com/photos/2695391/pexels-photo-2695391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+               title: 'Image 6'
+            },
+            {
+               link: 'https://images.pexels.com/photos/2065850/pexels-photo-2065850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+               title: 'Image 7'
+            },
+            {
+               link: 'https://images.pexels.com/photos/1535775/pexels-photo-1535775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+               title: 'Image 8'
+            },
+            {
+               link: 'https://images.pexels.com/photos/1754853/pexels-photo-1754853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+               title: 'Image 9'
             }
-         })
-         .then(function(response) {
-           vm.images = response.data;
-           console.log(response.data);
-         });
-      }]);
+         ];
+      });
 })();
+
 
  
